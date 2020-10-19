680253d94a88bdd092f5836fc0b3acc20e0d4789
pokrenz <pokrenz@gmail.com>
2020-10-19 23:48:36 +0700

    kuliner web

---
+++ precache-manifest.487ecbefb57a4e179678f70467a60fc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b72f3620250c18872fffdd91ed4883c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d396cc998778476ace3a",
+    "url": "/static/css/main.8afacc52.chunk.css"
+  },
+  {
+    "revision": "0b7b78f410eb82cdba2c",
+    "url": "/static/js/2.4348e4a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4348e4a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d396cc998778476ace3a",
+    "url": "/static/js/main.5eb582a0.chunk.js"
+  },
+  {
+    "revision": "5540e9f0a26e42bb168e",
+    "url": "/static/js/runtime-main.63b36f0c.js"
+  }
+]);
